98d916793ce07f7116eeff60b0f0886594a506b1
Supuni Wannigma <114279717+Supuni-Punsarani@users.noreply.github.com>
2024-03-04 22:53:18 +0530

    Project 1

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Project1.Person;
+
+namespace Project1
+{   
+   
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 a= new Class1();
+            a.Name = "Supuni";
+            a.Age = 23;
+            a.Haspet = true;
+
+            a.greeting();
+        }
+    }
+}
